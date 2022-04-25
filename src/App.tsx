114ca04d9a7bc7ea917add6ff5b7ab5e8737ddf7
Ernest Oliver Casanes <ernestolivercasanes@Ernests-MacBook-Air.local>
2022-04-25 23:58:53 +0800

    feat: init main route pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Browser from './pages/Browser';
+import Details from './pages/Details';
 
 function App() {
-  return <>App</>;
+  return (
+    <Routes>
+      <Route path="/" element={<Browser />} />
+      <Route path="/:id" element={<Details />} />
+    </Routes>
+  );
 }
 
 export default App;
